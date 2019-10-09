9d47ea075a1c17a5e19837fe243089af3884a8e8
Sager Williams <sagerwilliams0011@gmail.com>
2019-10-09 17:07:02 -0500

    Inquirer now lists products the user can purchase.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("dotenv").config();
 
+// connection options
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,8 +12,32 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+var productArr = [];
+
+// connect to database
 connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     console.log("Connected!");
+    connection.query("SELECT product_name FROM products", function (err, results) {
+        if (err) throw err;
+        
+        for (var i = 0; i < results.length; i++) {
+            productArr.push(results[i].product_name);
+        }
+        start();
+    });
 });
+
+function start() {
+    inquirer
+        .prompt({
+            name: "product",
+            type: "list",
+            message: "Which product would you like to buy?",
+            choices: productArr
+        })
+        .then(function (answer) {
+            
+        });
+}
