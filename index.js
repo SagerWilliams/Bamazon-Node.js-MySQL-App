0e9832ee58ae84804e6e04f447922e60b82828c4
Sager Williams <sagerwilliams0011@gmail.com>
2019-10-09 10:02:06 -0500

    Imported dotenv and established connection to database.

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // imports
 var mysql = require("mysql");
-var mysql = require("inquirer");
+var inquirer = require("inquirer");
+require("dotenv").config();
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.SQL_PASS,
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    console.log("Connected!");
+});
